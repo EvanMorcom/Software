b3a1d845acf7ed6e813299d97ff5e3804c5c4363
Evan <evan@localhost.localdomain>
2019-07-04 02:47:54 -0600

    First penalty goalie compiling. Untested

---
+++ src/thunderbots/software/ai/hl/stp/tactic/penalty_goalie.cpp
@@ -0,0 +1,5 @@
+//
+// Created by evan on 03/07/19.
+//
+
+#include "penalty_goalie.h"
---
+++ src/thunderbots/software/ai/hl/stp/tactic/penalty_goalie.h
@@ -0,0 +1,8 @@
+//
+// Created by evan on 03/07/19.
+//
+
+#ifndef PROJECT_PENALTY_GOALIE_H
+#define PROJECT_PENALTY_GOALIE_H
+
+#endif //PROJECT_PENALTY_GOALIE_H
